7570d72555c2e6e62634a6c2d0d878a512bb41aa
Magda Mejía <magda.mejia96@gmail.com>
2020-03-10 21:24:05 -0500

    Paso 8- Se agrega caso para probar el caso String con multiples numeros (Rojo)

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -3,17 +3,22 @@ from Calculator import Calculator
 
 class CalculatorTest(TestCase):
     def test_add(self):
-        self.assertEqual(Calculator().add(""),0,"Empty string")
+        self.assertEqual(Calculator().add(""), 0, "Empty string")
 
     def test_add_oneString(self):
-        self.assertEqual(Calculator().add("1"),1,"A number")
+        self.assertEqual(Calculator().add("1"), 1, "A number")
 
     def test_add_stringWithANumber(self):
-        self.assertEqual(Calculator().add("1"),1,"A number")
-        self.assertEqual(Calculator().add("2"),2,"A number")
+        self.assertEqual(Calculator().add("1"), 1, "A number")
+        self.assertEqual(Calculator().add("2"), 2, "A number")
 
     def test_add_stringWithTwoNumbers(self):
-        self.assertEqual(Calculator().add("1,3"),4,"Two numbers")
+        self.assertEqual(Calculator().add("1,3"), 4, "Two numbers")
+
+    def test_add_stringWithManyNumbers(self):
+        self.assertEqual(Calculator().add("1,3,4"), 8, "Two numbers")
+
+
 
 
 
