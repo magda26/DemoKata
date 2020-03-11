da32415aa8c3c021cdc2e4cd884b5a04819ec448
Magda Mejía <magda.mejia96@gmail.com>
2020-03-10 21:11:28 -0500

    Paso 5- Se agrega una prueba utilizando un numero diferente (Rojo)

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -5,6 +5,11 @@ class CalculatorTest(TestCase):
     def test_add(self):
         self.assertEqual(Calculator().add(""),0,"Empty string")
 
-    def test_add_onestring(self):
+    def test_add_oneString(self):
         self.assertEqual(Calculator().add("1"),1,"A Number")
+
+    def test_add_stringWithANumber(self):
+        self.assertEqual(Calculator().add("1"),1,"A Number")
+        self.assertEqual(Calculator().add("2"),2,"A Number")
+
 
