b178f6109e8b661d3f86deb111a7f24e211e39b8
Magda Mejía <magda.mejia96@gmail.com>
2020-03-10 21:21:31 -0500

    Paso 8- Se agrega codigo para probar test con dos numeros (Verde)

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,10 @@
 
 class Calculator:
-    def add(self,string):
+    def add(self, string):
         if string =="":
             return 0
+        elif "," in string:
+            return int(string[0]) + int(string[2])
         else:
             return int(string)
 
