251ef0ae65de3c4587e2ce7a76ab907e2114eecb
Magda Mejía <magda.mejia96@gmail.com>
2020-03-10 21:15:31 -0500

    Paso 7- Se agrega prueba para probar caso String con dos numeros (rojo)

--- CalculadoraTest.py
+++ CalculadoraTest.py
@@ -6,10 +6,14 @@ class CalculatorTest(TestCase):
         self.assertEqual(Calculator().add(""),0,"Empty string")
 
     def test_add_oneString(self):
-        self.assertEqual(Calculator().add("1"),1,"A Number")
+        self.assertEqual(Calculator().add("1"),1,"A number")
 
     def test_add_stringWithANumber(self):
-        self.assertEqual(Calculator().add("1"),1,"A Number")
-        self.assertEqual(Calculator().add("2"),2,"A Number")
+        self.assertEqual(Calculator().add("1"),1,"A number")
+        self.assertEqual(Calculator().add("2"),2,"A number")
+
+    def test_add_stringWithTwoNumbers(self):
+        self.assertEqual(Calculator().add("1,3"),4,"Two numbers")
+
 
 
